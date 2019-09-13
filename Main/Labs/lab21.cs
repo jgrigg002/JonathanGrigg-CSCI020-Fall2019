2278b8c8d9680fb00b49a2ddf59d92f9aff08b3e
jgrigg002 <jgrigg002@student.butte.edu>
2019-09-13 15:42:07 -0700

    lab2-1 work

--- Main/Labs/lab21.cs
+++ Main/Labs/lab21.cs
@@ -1,9 +1,23 @@
+// JonathanGrigg
+// 9.13.19
+// Animal question game
 using System;
 
 class MainClass
 {
   public static void Main ()
   {
-    Console.WriteLine ("Hello World");
+    // Purpose of the program is to ask questions using if else and switch statements
+    // to determin what animal the user is
+
+    //Variables
+    string[] animals = {"gold fish", "pirrahna", "manatee", "shark", "dolphin", "orca", "humpback", "blue whale"};
+    //test for string iteration
+    // for (int i = 0; i < 8; i++ )
+    // {
+    //   Console.WriteLine("\n" + i + "\n\n" + animals[i]);
+    // }
+    // test loop works succesfully
+
   }
 }
