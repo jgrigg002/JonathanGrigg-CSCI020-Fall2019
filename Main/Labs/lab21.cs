d19a17f7bcf4f64e85507c15fecd4fd9423ce673
jgrigg002 <jgrigg002@student.butte.edu>
2019-09-15 19:29:25 -0700

    final push for lab 2.1

--- Main/Labs/lab21.cs
+++ Main/Labs/lab21.cs
@@ -9,17 +9,23 @@ class MainClass
   {
     // Initialize variables
     // use string for storing answers
-    string question = "no";
+    // bool for setting up an ending
+    string question = null;
+    bool pass = false;
     // If statements
     // ask questions and store the asnwers to trigger if statements.
+    // boolean used to skip questions.
     Console.WriteLine("Are you a bird? \nYes or no?");
     question = Console.ReadLine();
     // Bird yes or no- fish yes or no - bug yes or no - mammal yes or no.
-    if (question == "yes"|| question == "Yes" || question == "y" || question == 'Y')
+    if (question == "yes"|| question == "Yes" || question == "y" || question == "Y")
     {
+      pass = true;
+      question = null;
       // Inside bird- are you flightless yes or no
       Console.WriteLine("Are you flightless \nYes or no?");
-      if (question == "yes"|| question == "Yes" || question == "y" || question == 'Y')
+      question = Console.ReadLine();
+      if (question == "yes"|| question == "Yes" || question == "y" || question == "Y")
       {
         Console.WriteLine("You might be a penguin.");
       }
@@ -29,28 +35,44 @@ class MainClass
       }
     }
     // are you a fish
-    Console.WriteLine("Are you a fish");
-    question = Console.ReadLine();
-    if (question == "yes"|| question == "Yes" || question == "y" || question == 'Y')
+    if (pass == false)
     {
-      Console.WriteLine("You might be a salmon.");
+      Console.WriteLine("Are you a fish");
+      question = Console.ReadLine();
+
+      if (question == "yes"|| question == "Yes" || question == "y" || question == "Y")
+      {
+        pass = true;
+        Console.WriteLine("You might be a salmon.");
+      }
     }
     //
-    Console.WriteLine("Are you a bug");
-    question = Console.ReadLine();
-    // are you a bug question
-    if (question == "yes"|| question == "Yes" || question == "y" || question == 'Y')
+    if (pass == false)
     {
-      Console.WriteLine("You might be a lady bug.");
+      Console.WriteLine("Are you a bug");
+      question = Console.ReadLine();
+    // are you a bug question
+      if (question == "yes"|| question == "Yes" || question == "y" || question == "Y")
+      {
+        pass = true;
+        Console.WriteLine("You might be a lady bug.");
+      }
     }
     //
     // Inside mammal- the scope of mammals is to large for this program. congratulations.
-    Console.WriteLine("Are you a mammal?");
-    question = Console.ReadLine();
-    if (question == "yes"|| question == "Yes" || question == "y" || question == 'Y')
+    if (pass == false)
     {
-      Console.WriteLine("Good for you.");
+      Console.WriteLine("Are you a mammal?");
+      question = Console.ReadLine();
+      if (question == "yes"|| question == "Yes" || question == "y" || question == "Y")
+      {
+        pass = true;
+        Console.WriteLine("Good for you.");
+      }
     }
-    else Console.WriteLine("Youre not an animal then");
+    if (pass == false)
+    {
+     Console.WriteLine("You're not an animal then");
+    }
   }
 }
