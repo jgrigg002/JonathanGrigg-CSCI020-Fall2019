98fa1e0aa2bd9d403aa1ac4153fae0784fff2130
jgrigg002 <jgrigg002@student.butte.edu>
2019-09-15 19:02:18 -0700

    push before class

--- Main/Labs/lab21.cs
+++ Main/Labs/lab21.cs
@@ -7,17 +7,50 @@ class MainClass
 {
   public static void Main ()
   {
-    // Purpose of the program is to ask questions using if else and switch statements
-    // to determin what animal the user is
-
-    //Variables
-    string[] animals = {"gold fish", "pirrahna", "manatee", "shark", "dolphin", "orca", "humpback", "blue whale"};
-    //test for string iteration
-    // for (int i = 0; i < 8; i++ )
-    // {
-    //   Console.WriteLine("\n" + i + "\n" + animals[i]);
-    // }
-    // test loop works succesfully
-
+    // Initialize variables
+    // use string for storing answers
+    string question ;
+    // If statements
+    // ask questions and store the asnwers to trigger if statements.
+    Console.WriteLine("Are you a bird? \nYes or no?");
+    question = Console.ReadLine();
+    // Bird yes or no- fish yes or no - bug yes or no - mammal yes or no.
+    if (question == "yes"|| question == "Yes" || question == "y" || question == 'Y')
+    {
+      // Inside bird- are you flightless yes or no
+      Console.WriteLine("Are you flightless \nYes or no?");
+      if (question == "yes"|| question == "Yes" || question == "y" || question == 'Y')
+      {
+        Console.WriteLine("You might be a penguin.");
+      }
+      else
+      {
+        Console.WriteLine("You might be a sparrow.");
+      }
+    }
+    // are you a fish
+    Console.WriteLine("Are you a fish");
+    question = Console.ReadLine();
+    if (question == "yes"|| question == "Yes" || question == "y" || question == 'Y')
+    {
+      Console.WriteLine("You might be a salmon.");
+    }
+    //
+    Console.WriteLine("Are you a bug");
+    question = Console.ReadLine();
+    // are you a bug question
+    if (question == "yes"|| question == "Yes" || question == "y" || question == 'Y')
+    {
+      Console.WriteLine("You might be a lady bug.");
+    }
+    //
+    // Inside mammal- the scope of mammals is to large for this program. congratulations.
+    Console.WriteLine("Are you a mammal?");
+    question = Console.ReadLine();
+    if (question == "yes"|| question == "Yes" || question == "y" || question == 'Y')
+    {
+      Console.WriteLine("Good for you.");
+    }
+    else Console.WriteLine("Youre not an animal then");
   }
 }
