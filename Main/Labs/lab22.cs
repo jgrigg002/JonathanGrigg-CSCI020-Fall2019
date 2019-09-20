ae5510832ecbb52962460678bea49108912d0f3d
jgrigg002 <jgrigg002@student.butte.edu>
2019-09-19 19:13:42 -0700

    backup push

---
+++ Main/Labs/lab22.cs
@@ -0,0 +1,15 @@
+//JonathanGrigg
+//9.19.19
+//updated animal
+using System;
+class MainClass{
+  public static void Main (){
+    //use switches to set off chain questions
+    //use string for storing answers
+    string promptAnswer = null;
+    //giest is to promt the user for what animal the are and then input their answer into the string
+    Console.WriteLine("Welcome to the animal guessing game \nWhere I ask you a question, you answer and then I guess what animal you are in a very poor manner.\nPlease type your answer exactly as it's written");
+    promptAnswer = Console.ReadLine();
+    
+  }
+}
