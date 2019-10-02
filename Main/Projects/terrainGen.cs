8f6ffc63b165860457c8404001314528d8c1e5ce
jgrigg002 <jgrigg002@student.butte.edu>
2019-10-01 18:57:26 -0700

    main proj sudo push

---
+++ Main/Projects/terrainGen.cs
@@ -0,0 +1,20 @@
+//Jonathan Grigg
+//final project
+//procedural generator
+//uses random or input seed to create a map and then export as an ascii
+
+// create main stuff
+// create a two dimension array based on user input for size of map
+// user input for proportions
+possibly let user input seed that where each value determines what's in map
+
+// set each value to whitespace
+// function using vectors to change values in map along directions
+// random start points along edges for mountains and hills.
+// create mountains as general directions with a variable thickness
+// then flank by hills on one or both sides based on chance
+// River starts at mountains and change hills measure directions and then travel perpendicular
+// lakes start at random river point and fill.
+// place house with wall.
+//
+// print whole thing to .txt file.
