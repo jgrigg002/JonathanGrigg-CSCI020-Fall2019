43b273db945d7ab56acd0bb68206038c5ab0d8b6
jgrigg002 <jgrigg002@student.butte.edu>
2019-10-09 12:38:02 -0700

    Code update.

--- Main/Labs/lab27Venus.cs
+++ Main/Labs/lab27Venus.cs
@@ -4,30 +4,32 @@
 //venus location tester
 
 using System;
+using System.IO;
 class MeteorTest {
   public static void Main(){
     //create string array to read data into
     string inputString;
-    string[] sortingString;
     double[] sortingDouble = new double[3];
     //open read and write fnctions
     StreamReader reader = new StreamReader("venus.csv");
-    StreamWriter write;
+    StreamWriter writer;
     writer = new StreamWriter("venusPost.csv");
     inputString = reader.ReadLine();
     while (reader.EndOfStream == false)
     {
       inputString = reader.ReadLine();
       //create loop to read in a line starting at 2nd line. Using split.
-      sortingString = inputString.split(",");
-      sortingDouble[0] = double.Parse.sortingString[3];
-      sortingDouble[1] = double.Parse.sortingString[4];
-      sortingDouble[2] = double.Parse.sortingString[5];
+      string[] sortingString = inputString.Split(",");
+      sortingDouble[0] = Double.Parse(sortingString[2]);
+      sortingDouble[1] = Double.Parse(sortingString[3]);
+      sortingDouble[2] = Double.Parse(sortingString[4]);
       //inset a loop to read the locations and then parse them into a double array.
+      //after the read in test the double's values and then write the string array if true.
       if (sortingDouble[0] >= -40 && sortingDouble[0] <= 50 && sortingDouble[1] >= 40 && sortingDouble[1] >= 135 && sortingDouble[2] >=60){
+        Console.WriteLine(sortingString);
         writer.WriteLine(sortingString);
       }
-      //after the read in test the double's values and then write the string array if true.
+
       //close the read and write fnctions
     }
     reader.Close();
